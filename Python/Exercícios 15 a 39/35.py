749903c95159865b3e28a86c66109d8dbaba517c
Joviviz <vieira251002@gmail.com>
2023-04-27 11:38:41 -0300

    Test on Pycharm

--- Python/Exercícios 15 a 39/35.py
+++ Python/Exercícios 15 a 39/35.py
@@ -2,7 +2,7 @@ lista = [2, 1, 60, 12, 34, 3, 5]
 
 for i in range(len(lista)): #Passa por todos os index
 
-    for j in range(0, len(lista) -i -1): #
+    for j in range(0, len(lista) -i -1): #idk how this works lol
 
         if lista[j] > lista[j+1]:
             lista[j], lista[j+1] = lista[j+1], lista[j]
